8a16433011c622a294a11b41e3178a43fa077240
Bryan Workman <bryan@bryans-mbp.lan>
2020-12-28 20:33:25 -0800

    start project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Bryan Workman on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
